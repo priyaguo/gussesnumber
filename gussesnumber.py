0fc3f8230b3ecf74b0f7e80c6f112e57770a7c7e
Priya Guo <priya.guo@gmail.com>
2021-05-06 14:34:46 +0800

    second version

--- gussesnumber.py
+++ gussesnumber.py
@@ -1,16 +1,19 @@
 #猜數字遊戲
 import random
-r = random.randint(1,100)
 
 input('====猜數字遊戲====')
 
-
+r = random.randint(1,100)
+count = 0
 while True:
-    gussesnumber = int(input('請輸入1~100的整數:'))
-    if gussesnumber == r:
+    count += 1  # count = count + 1
+    num = int(input('請輸入1~100的整數:'))
+    if num == r:
         print('終於猜對了!!恭喜你!!')
+        print('這是你猜的第',count,'次')
         break
-    elif gussesnumber > r:
+    elif num > r:
         print('你猜錯了!!你輸入的數字比較大!!')
-    else:
+    elif num < r:
         print('你猜錯了!!你輸入的數字比較小!!')
+    print('這是你猜的第',count,'次')
